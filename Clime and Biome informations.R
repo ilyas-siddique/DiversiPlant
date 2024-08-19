93dca880102a36fb05d137f168863f1e4442a4de
renata-lucas <renata.lucas85@gmail.com>
2024-08-19 14:27:14 -0300

    Update Clime and Biome informations.R

--- Clime and Biome informations.R
+++ Clime and Biome informations.R
@@ -4,10 +4,10 @@ View(biomes_data)
 misc_env <- GIFT_env_meta_misc(
 	api = "https://gift.uni-goettingen.de/api/extended/",
 	GIFT_version = "latest")
-View(misc_env)# list of environment variables
+View(misc_env) # list of environment variables
 
 raster_env <- GIFT_env_meta_raster()
-View(raster_env)#list of climatic variables
+View(raster_env) #list of climatic variables
 
 env_data <- GIFT_env(
 	entity_ID = NULL,
@@ -15,16 +15,15 @@ env_data <- GIFT_env(
 	rasterlayer = NULL,
 	GIFT_version = "latest",
 	api = "https://gift.uni-goettingen.de/api/extended/")
-View(env_data)#geo_entity and biome for each
+
 
 clim_data <- GIFT_env(entity_ID = NULL,
                     miscellaneous = "biome",
-                    rasterlayer = c("wc2.0_bio_30s_12", "wc2.0_bio_30s_01"))#mostrar acumulada e não a maedia
-View(clim_data)
-
-clim_biom <- merge(biomes_data, clim_data[-c(2)], by = "entity_ID")
-View(clim_biom)#mean temperature, mean annual precipitation, geographic coordinates, biomes, botanical country and so on variables
+                    rasterlayer = c("wc2.0_bio_30s_12", "wc2.0_bio_30s_01")) 
+	#need add overlap and entity_type selection command, and check if this is the mean or accumulated data
 
 
+clim_biom <- merge(biomes_data, clim_data[-c(2)], by = "entity_ID") #mean temperature, mean annual precipitation, geographic coordinates, biomes, botanical country and so on variables
 
-GIFT_species_distribution()
+
+
